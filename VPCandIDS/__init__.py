57459053d1424b798993f044f7889e08c00f4059
safenein <safenein@protonmail.com>
2024-06-04 09:16:56 +0200

    fix: Global logging

---
+++ VPCandIDS/__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+from . import logger
+
+logger.enable()
+log = logging.getLogger(__name__)
--- VPCandIDS/__main__.py
+++ VPCandIDS/__main__.py
@@ -1,8 +1,2 @@
 import boto3
-import logging
-
-from . import logger
-
-logger.enable()
-log = logging.getLogger(__name__)
 
