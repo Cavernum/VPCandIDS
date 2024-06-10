a76732678c638df46ff15ffda3fa49a7b25c3668
Lasmaw <mateo.lachaize@gmail.com>
2024-06-10 17:22:28 +0200

    feat: Adding key support

--- VPCandIDS/EC2/__init__.py
+++ VPCandIDS/EC2/__init__.py
@@ -65,17 +65,43 @@ def create_ubuntu_instance(name, subnet_id: str, security_groups_ids: list[str],
     return instance
 
 
-def download_key(instance):
-    ec2=instance
+def download_key(file_name,path):
+    path = os.path.realpath(path)
+    ec2 = boto3.client("ec2")
     # Nom de la nouvelle paire de clés
-    key_pair_name = 'key'
+    key_pair_name = file_name
+        # Vérifier si la paire de clés existe déjà
+    try:
+        existing_key_pairs = ec2.describe_key_pairs(KeyNames=[key_pair_name])
+        if existing_key_pairs['KeyPairs']:
+            log.warn("La paire de clés existe déjà. Elle va être supprimée.")
+            # Supprimer la paire de clés existante
+            ec2.delete_key_pair(KeyName=key_pair_name)
+            log.info(f"La paire de clés existante '{key_pair_name}' a été supprimée.")
+    except ec2.exceptions.ClientError as e:
+        if 'InvalidKeyPair.NotFound' in str(e):
+            log.info(f"Aucune paire de clés existante trouvée avec le nom '{key_pair_name}'.")
+        else:
+            log.info(f"Erreur lors de la vérification de la paire de clés: {e}")
+            return
     # Créer une nouvelle paire de clés
     response = ec2.create_key_pair(KeyName=key_pair_name)
     # Récupérer la clé privée PEM
     key_material = response['KeyMaterial']
     # Sauvegarder la clé privée dans un fichier .pem
-    with open(f'{key_pair_name}.pem', 'w') as file:
+    with open(os.path.join(path, f"{key_pair_name}.pem"), 'w') as file:
         file.write(key_material)
     # Modifier les permissions du fichier pour des raisons de sécurité
-    os.chmod(f'{key_pair_name}.pem', 0o400)
-    print(f"La nouvelle clé {key_pair_name}.pem a été créée.")
+    os.chmod(f'{path}/{key_pair_name}.pem', 0o400)
+    log.info(f"La nouvelle clé {key_pair_name}.pem a été créée.")
+
+def search_key(file_name):
+    path = os.path.realpath('./KEYS')
+    contents = os.listdir(path)
+    for e in contents:
+        if e==f"{file_name}.pem":
+            log.info(f"La paire de clé {file_name} a été trouvée.")
+            return e
+    log.warn(f"La paire de clé {file_name} n'a pas été trouvée.")
+
+    
